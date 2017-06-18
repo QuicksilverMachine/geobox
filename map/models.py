615ec835bdf83cd7ca46f11ef48d070f72c39b7b
dejan <quicksilver.machine@gmail.com>
2017-06-18 19:23:59 +0200

    Update models

--- map/models.py
+++ map/models.py
@@ -6,8 +6,15 @@ from accounts.models import User
 
 
 class Map(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
+    id = models.TextField(primary_key=True)
     title = models.CharField(_("Title"), max_length=100)
     description = models.TextField(_("Description"))
     created = models.DateTimeField(_("Created"), default=now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Waypoint(models.Model):
+    id = models.IntegerField(primary_key=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
