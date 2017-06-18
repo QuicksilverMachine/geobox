88aba4ddd13b6bfb11a26404a1a3680e61346a86
dejan <quicksilver.machine@gmail.com>
2017-06-18 23:34:12 +0200

    Create map crud operations and permissions

--- map/urls.py
+++ map/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     url(r'^(?P<pk>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}'
         r'-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})$',
         views.MapDetailView.as_view(), name='detail'),
+    url(r'^create/$', views.create_map, name='create'),
+    url(r'^update/(?P<pk>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}'
+        r'-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})$',
+        views.update_map,
+        name='update'),
+    url(r'^delete/(?P<pk>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}'
+        r'-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})$',
+        views.delete_map,
+        name='delete'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,5 +1,10 @@
-from django.views import generic
-from map.models import Map
+import uuid
+
+from django.http import HttpResponse, Http404
+from django.shortcuts import redirect
+from django.views import generic, View
+
+from map.models import Map, Waypoint
 
 
 class MapIndexView(generic.ListView):
@@ -7,11 +12,91 @@ class MapIndexView(generic.ListView):
     context_object_name = 'maps'
 
     def get_queryset(self):
-        media = Map.objects.all()
-        return media
+        maps = Map.objects.all()
+        return maps
 
 
 class MapDetailView(generic.DetailView):
     model = Map
     template_name = 'map/map.html'
     context_object_name = 'map'
+
+    def get_object(self, queryset=None):
+
+        queried_map = super(MapDetailView, self).get_object()
+        if self.request.user != queried_map.user and queried_map.private:
+            raise Http404
+        return queried_map
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if context['map'].user != self.request.user:
+            context['map'].editing = False
+        context['waypoints'] = Waypoint.packed(context['map'].id)
+        return context
+
+
+def create_map(request):
+    if not request.user.is_authenticated.value:
+        return HttpResponse(status=403)
+
+    new_map_id = uuid.uuid4()
+    user_maps_count = request.user.map_set.count()
+    new_map = Map(
+        id=new_map_id,
+        user=request.user,
+        title="Map-{}".format(user_maps_count + 1),
+    )
+    new_map.save()
+    return redirect('/map/{}'.format(new_map_id))
+
+
+def delete_map(request, pk):
+    if not request.user.is_authenticated.value:
+        return HttpResponse(status=404)
+
+    map_to_delete = Map.objects.get(id=pk)
+
+    if request.user != map_to_delete.user:
+        return HttpResponse(status=404)
+
+    map_to_delete.delete()
+    return redirect('/map/')
+
+
+def update_map(request, pk):
+    if not request.user.is_authenticated.value:
+        return HttpResponse(status=404)
+
+    map_to_update = Map.objects.get(id=pk)
+
+    if request.user != map_to_update.user:
+        return HttpResponse(status=404)
+
+    map_to_update.title = request.POST.get('title', map_to_update.title)
+    map_to_update.description = request.POST.get('description',
+                                                 map_to_update.description)
+    map_to_update.editing = True if request.POST.get('editing') else False
+    map_to_update.private = True if request.POST.get('private') else False
+    map_to_update.save()
+
+    packed_waypoints = request.POST.get('waypoints', "")
+    new_waypoints = [wp for wp in packed_waypoints.split('|') if wp]
+
+    try:
+        # Delete old waypoints
+        old_waypoints = Waypoint.objects.filter(map__user=request.user)
+        old_waypoints.delete()
+    except Waypoint.DoesNotExist:
+        pass
+
+    for wp in new_waypoints:
+        lat_lng = wp.split('/')
+        waypoint = Waypoint(
+            latitude=lat_lng[0],
+            longitude=lat_lng[1],
+            map=map_to_update
+        )
+        waypoint.save()
+
+    return redirect('/map/{}'.format(pk))
